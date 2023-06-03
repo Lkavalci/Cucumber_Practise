36192f2444419e020c93f0e8a10a2aad4e77a704
Lkavalci <lutfikavalci@gmail.com>
2023-06-03 22:01:52 +0300

    230603

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,27 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {"html:target/cucumber-reports.html",  // raporun adının ne olarak cıkmasını istiyorsak cucumber-reports --> ismini degistirebilirsiniz.
+                    "json:target/json-reports/cucumber.json", // rapor sonucunu json formatında istenirse yapilir. cıkan sonuc kopyalanır browser dan jsonpath.com a gidilip yapistirilir.
+                    "junit:target/xml-report/cucumber.xml" // xml rapor hazırlamak için kullanilir. json ve xml makina diliyle rapor yazar. herkes istemez pek kuılanilmaz. bazı işverenler isteyebilir.
+            },
+            features = "src/test/resources/features",
+            glue = "stepdefinitions",
+            tags = "@ae",
+
+            dryRun = false     // eksik adim varmi diye kontrol ettigimizde "false" i "true" yapiyoruz.
+            // sonuc pass yazsada cikan sonuc test'in degil, yapilandirma nin yapildini gosterir.
+    )
+
+    public class Runner {
+}
